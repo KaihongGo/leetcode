ad690938595c8a9adf0d069e7b3c9aa6af7aa1e2
Kaihong Yan <34575284+KaihongGo@users.noreply.github.com>
2020-10-09 16:32:19 +0800

    init repository

---
+++ 155-Min Stack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class MinStack
+{
+public:
+    /** initialize your data structure here. */
+    MinStack()
+    {
+    }
+
+    void push(int x)
+    {
+    }
+
+    void pop()
+    {
+    }
+
+    int top()
+    {
+    }
+
+    int getMin()
+    {
+    }
+
+private:
+    unsigned int top = -1;
+    unsigned int min = -1;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
